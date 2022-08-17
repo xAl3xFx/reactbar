41bd25c895c575ad5912b2a472061eba3d186898
Alex Petrov <alex.petrov@inerps.eu>
2022-08-17 15:36:20 +0300

    rollup - Do not use css modules.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export * from './sidebar/Sidebar'
 export * from './sidebar/Topbar'
-export * from './sidebar/sidebar.css'
-export * from './sidebar/topbar.css'
--- tsdx.config.js
+++ tsdx.config.js
@@ -2,9 +2,7 @@ const postcss = require('rollup-plugin-postcss');
 module.exports = {
     rollup(config, options) {
         config.plugins.push(
-            postcss({
-                modules: true,
-            })
+            postcss()
         );
         return config;
     },
