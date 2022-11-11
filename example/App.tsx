8edc7d252880f166d5df9f317d7f789be6aede36
Alex Petrov <alex.petrov@inerps.eu>
2022-11-11 16:36:40 +0200

    Fixed import

--- example/App.tsx
+++ example/App.tsx
@@ -1,5 +1,5 @@
 import * as React  from 'react';
-import {SidebarOld, SidebarItem} from "../src";
+import {SidebarItem} from "../src";
 import './index.css'
 import {Sidebar} from "../src/sidebar/Sidebar";
 
